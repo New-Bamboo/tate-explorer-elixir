352fb77257d7e90c2c042b4b6aac325d99f6d182
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-12-04 11:45:06 +0000

    Adds artists migration.

---
+++ priv/repo/migrations/20131204113925_create_artists.exs
@@ -0,0 +1,24 @@
+defmodule TateExplorer.Repo.Migrations.CreateArtists do
+  use Ecto.Migration
+
+  def up do
+    """
+    CREATE TABLE artists (
+      id serial,
+      name text,
+      gender text, -- TODO: change this to proper enum when possible
+      dates text,
+      year_of_birth date,
+      year_of_death date,
+      place_of_death text,
+      url text
+    );
+    """
+  end
+
+  def down do
+    """
+    DROP TABLE artists;
+    """
+  end
+end
