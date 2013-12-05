4938f914f8e3fb439f5dd8310156c39eb573a8f6
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-12-05 15:11:04 +0000

    Uses env and app specific Redis keys.

--- lib/tate_explorer/cache.ex
+++ lib/tate_explorer/cache.ex
@@ -1,10 +1,10 @@
 defmodule TateExplorer.Cache do
   def store(key, data) do
-    client |> Exredis.query ["SET", key, data]
+    client |> Exredis.query ["SET", key_with_metadata(key), data]
   end
 
   def get(key) do
-    client |> Exredis.query ["GET", key]
+    client |> Exredis.query ["GET", key_with_metadata(key)]
   end
 
   def client do
@@ -15,5 +15,9 @@ defmodule TateExplorer.Cache do
     { :ok, client } = Exredis.start_link
     Process.register(client, :redis_client)
     { :ok, client }
+  end
+
+  defp key_with_metadata(key) do
+    "tate-explorer-#{Mix.env}-#{key}"
   end
 end
