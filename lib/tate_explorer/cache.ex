c9a016bdbd61039be3ee3b286ad80b70db2d2525
Claudio Ortolina <claudio@new-bamboo.co.uk>
2013-12-06 09:17:10 +0000

    Allows custom Redis connection for prod.

--- lib/tate_explorer/cache.ex
+++ lib/tate_explorer/cache.ex
@@ -12,7 +12,7 @@ defmodule TateExplorer.Cache do
   end
 
   def start_link do
-    { :ok, client } = Exredis.start_link
+    { :ok, client } = start_redis_server(Mix.env)
     Process.register(client, :redis_client)
     { :ok, client }
   end
@@ -20,4 +20,17 @@ defmodule TateExplorer.Cache do
   defp key_with_metadata(key) do
     "tate-explorer-#{Mix.env}-#{key}"
   end
+
+  defp start_redis_server(:prod) do
+    Exredis.start_link(
+      System.env("REDIS_HOST"),
+      System.env("REDIS_PORT"),
+      System.env("REDIS_DATABASE"),
+      System.env("PASSWORD")
+    )
+  end
+  defp start_redis_server(_) do
+    Exredis.start_link
+  end
+
 end
